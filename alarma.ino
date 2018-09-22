b7521c1010431c62843004c6dabfa33368b5f33c
Jorge Guani <jorge_guani@hotmail.com>
2018-09-22 12:35:54 -0500

    Agregadodel boton para la interrupción

--- alarma.ino
+++ alarma.ino
@@ -1,16 +1,37 @@
+/*
+ * Sistema Notificador de Llegada de Individuos
+ * Autor: Jorge Enrique Aguado Guaní
+*/
+
+/* Creación de variables para los pines: */
 #define buzzer 6
+#define boton 2
+#define led 3
+
+boolean estadoBoton = false;
 
 void setup() {
-  pinMode(buzzer, OUTPUT);
+  pinMode(boton, INPUT);
+  pinMode(buzzer, OUTPUT); //pin del buzzer como salida
+  attachInterrupt(digitalPinToInterrupt(boton), interrupcion, RISING);
+  Serial.begin(9600);
 }
 
 void loop() {
-  alarma();
+  alarma(); //activar alarma
 }
 
-void alarma() {
-  tone(buzzer, 1000);
-  delay(100);
-  noTone(buzzer);
-  delay(100);
+void alarma() { //método para hacer sonar la alarma
+  /*Secuencia o ritmo de la alarma: */
+  while(estadoBoton) {
+    tone(buzzer, 1000); //enviar 1kHz de señal
+    delay(100);         
+    noTone(buzzer);
+    delay(100);
+  }
+}
+
+void interrupcion() {
+  delay(100000);
+  estadoBoton = !estadoBoton; //switchear el estado del boton
 }
