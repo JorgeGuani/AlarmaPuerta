874c79a8e3c8489c5f09a796153eebcefeea65e8
Jorge Guani <jorge_guani@hotmail.com>
2018-09-27 00:17:54 -0500

    Añadido de un cuarto ritmo de la alarma

--- alarma.ino
+++ alarma.ino
@@ -6,29 +6,26 @@
  * 7° semestre
 */
 
-/***************************VARIABLES***************************/
 /* Creación de variables para los pines: */
-#define buzzer 6 //bocina
-#define boton 2 //abrir/cerrar puerta
+#define buzzer 5          //bocina
+#define boton 2           //abrir/cerrar puerta
 #define botonSecuencia 3  //cambiar secuencia rítmica de la alarma
-#define ledAlarma 7 //led inidicador de alarma activa
+#define ledAlarma 4       //led inidicador de alarma activa
 
 /* Variables para los leds del display de 7 segmentos*/
-#define leda 8  //a del display
-#define ledb 9  //b del display
-#define ledc 10 //c del display
-#define ledd 11 //d del display
-#define lede 12 //e del display
-#define ledg 5  //g del display
+#define leda 6    //a del display
+#define ledb 7    //b del display
+#define ledc 8    //c del display
+#define ledd 9    //d del display
+#define lede 10   //e del display
+#define ledf 11   //f del display
+#define ledg 12   //g del display
 
-boolean estadoBoton = false; //alternar entre abrir/cerrar
-int ritmoAlarma = 1; //tipo de ritmo secuencia
-/****************************************************************/
+boolean estadoBoton = false;  //alternar entre abrir/cerrar
+int ritmoAlarma = 1;          //tipo de ritmo secuencia
+int numeroAleatorio;          //frecuencia alarma 4
 
-
-/*********************CONFIGURACIÓN INICIAL**********************/
 void setup() {
-  //Pines de entrada y salida
   pinMode(boton, INPUT);
   pinMode(botonSecuencia, INPUT);
   pinMode(buzzer, OUTPUT);
@@ -38,45 +35,54 @@ void setup() {
   pinMode(ledc, OUTPUT);
   pinMode(ledd, OUTPUT);
   pinMode(lede, OUTPUT);
+  pinMode(ledf, OUTPUT);
   pinMode(ledg, OUTPUT);
   
   //Interrupciones para cada botón
   attachInterrupt(digitalPinToInterrupt(boton), 
                   abrirCerrarPuerta, 
                   RISING);
+                  
   attachInterrupt(digitalPinToInterrupt(botonSecuencia), 
                   cambioDeSecuencia, 
                   RISING); 
                                  
-  Serial.begin(9600); //ajustar el monitor serie a 9600
-  apagarLedsDisplay(); //limpia el display
-  display1(); //ritmo 1 predeterminado (ritmo inicial)
+  Serial.begin(9600);   //ajustar el monitor serie a 9600
+  apagarLedsDisplay();  //limpia el display
+  display1();           //ritmo 1 predeterminado (ritmo inicial)
 }
-/*****************************************************************/
 
 
-/*********************ESTRUCTURA FUNCIONAL**********************/
 void loop() {
   while(estadoBoton) { //Mientras esté activa la alarma
-    if(ritmoAlarma == 1) { //Si está activo el ritmo 1
-      alarma1(); 
-    }
-    else if(ritmoAlarma == 2) {
-      alarma2(); 
-    }
-    else if(ritmoAlarma == 3) {
-      alarma3();
+    switch(ritmoAlarma) { //irá al número de ritmo correspondiente
+      case 1:
+        alarma1();
+        break;
+        
+      case 2:
+        alarma2();
+        break;
+        
+      case 3:
+        alarma3();
+        break;
+        
+      case 4:
+        alarma4();
+        break;  
     }
     Serial.println("Estoy trabajando"); //el arduino está activo
   }
   Serial.println("Zzz..."); //El arduino está en modo bajo consumo
 }
 
+
 /* MÉTODOS PARA CADA RITMO DE LA ALARMA */
-void alarma1() { //para hacer sonar la alarma 1
+void alarma1() {    //para hacer sonar la alarma 1
   /*Secuencia o ritmo de la alarma 1: */
   noTone(buzzer);
-  tone(buzzer, 1000); //enviar 1kHz de señal
+  tone(buzzer, 1000);             //enviar 1kHz de señal
   digitalWrite(ledAlarma, HIGH);
   delay(100);         
   
@@ -85,16 +91,17 @@ void alarma1() { //para hacer sonar la alarma 1
   delay(100);
 }
 
-void alarma2() { //para hacer sonar la alarma 2
+void alarma2() {    //para hacer sonar la alarma 2
   /*Secuencia o ritmo de la alarma 2: */
   noTone(buzzer);
-  tone(buzzer, 950); //enviar 950 Hz de señal
+  tone(buzzer, 950);
   digitalWrite(ledAlarma, HIGH);
   delay(100);         
   
   noTone(buzzer);
   digitalWrite(ledAlarma, LOW);
-  delay(100);    
+  delay(100);
+      
   tone(buzzer, 950);
   digitalWrite(ledAlarma, HIGH);
   delay(100);             
@@ -104,16 +111,31 @@ void alarma2() { //para hacer sonar la alarma 2
   delay(400);
 }
 
-void alarma3() { //Para hacer sonar la alarma 3
+void alarma3() {  //Para hacer sonar la alarma 3
   /*Secuencia o ritmo de la alarma 3: */
   noTone(buzzer);  
-  tone(buzzer, 1000); //enviar 1kHz de señal
+  tone(buzzer, 1000);   //enviar 1kHz de señal
   digitalWrite(ledAlarma, HIGH);
   delay(2000);         
   
   noTone(buzzer);
   digitalWrite(ledAlarma, LOW);
   delay(2000);
+}
+
+void alarma4() { //Para hacer sonar la alarma 4
+  /*Secuencia o ritmo de la alarma 4 (melodía): */
+  randomSeed(analogRead(0)); // genera una semilla para el núm. aleatorio
+  numeroAleatorio = random(523, 1479); //para la frecuencia
+  
+  noTone(buzzer);  
+  tone(buzzer, numeroAleatorio);       //frecuencia aleatoria
+  digitalWrite(ledAlarma, HIGH);
+  delay(map(numeroAleatorio, 523, 1479, 200, 500)); //duración aleatoria        
+  
+  noTone(buzzer);
+  digitalWrite(ledAlarma, LOW);
+  delay(map(numeroAleatorio, 523, 1479, 200, 400)); //espera aleatoria        
 }
 
 /* MÉTODOS PARA MOSTRAR EN EL DISPLAY CADA NÚMERO*/
@@ -138,29 +160,50 @@ void display3() {
   digitalWrite(ledg, LOW);
 }
 
+void display4() {
+  digitalWrite(ledf, LOW);
+  digitalWrite(ledg, LOW);
+  digitalWrite(ledb, LOW);
+  digitalWrite(ledc, LOW);
+}
+
 /* MÉTODO PARA APAGAR LOS LEDS DEL DISPLAY*/
 void apagarLedsDisplay() {
-  for(int i = 8; i <= 12; i++) {
+  for(int i = 6; i <= 12; i++) {
     digitalWrite(i, HIGH);
   }
-  digitalWrite(ledg, HIGH);
 }
 
 /*MÉTODOS DE LAS INTERRUPCIONES: */
+/*Interrupción 1*/
 void abrirCerrarPuerta() { //Botón 1 (abrir/cerrar puerta)
   noTone(buzzer);
   digitalWrite(ledAlarma, LOW);
   estadoBoton = !estadoBoton; //Abrir/cerrar puerta
 }
 
+/*Interrupción 2*/
 void cambioDeSecuencia() {
-  ritmoAlarma++; //cambia la secuencia rítmica
-  if(ritmoAlarma > 3) ritmoAlarma = 1; //reiniciar conteo de secuencia de tono
-  
-  apagarLedsDisplay(); //limpia el display
-  
-  if(ritmoAlarma == 1) display1();  //valor del display es el número de ritmo
-  else if(ritmoAlarma == 2) display2();
-  else if(ritmoAlarma == 3) display3();
+  ritmoAlarma++;          //cambia la secuencia rítmica
+  if(ritmoAlarma > 4) ritmoAlarma = 1;  //reiniciar conteo de secuencia de tono 
+   
+  apagarLedsDisplay();    //limpia el display
+
+  switch(ritmoAlarma) {   //el valor del display es el número de ritmo
+    case 1:
+      display1();
+      break;
+
+    case 2:
+      display2();
+      break;
+
+    case 3:
+      display3();
+      break;
+
+    case 4:
+      display4();
+      break;
+  }
 }
-/********************************************************************************/
